218f210f72fc3f3c7b6fd9f7055806ea1e95b019
ikxo3 <161986480+ikxo3@users.noreply.github.com>
2026-02-05 13:13:17 +0100

    Obscure secret key in settings.py
    Replaced the actual secret key with a placeholder.

--- skinAI/settings.py
+++ skinAI/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY='django-insecure-z3(gm(xp8h083*5y#@el_9n8==x)9i_vu1j0wo(g8f74(%^4z#'
+SECRET_KEY='***'
 DEBUG=True
 
 ALLOWED_HOSTS = ['*']
